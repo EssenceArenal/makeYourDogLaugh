0ae6fb3d4b4a91622e70651bf95c8acecc7176c6
EssenceArenal <essencearenalcr@gmail.com>
2025-08-24 10:59:03 -0600

    Update index.tsx

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { auth } from "../../src/lib/firebase";
 import { saveUserData } from "../../src/lib/airtable";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import contentJson from "../../data/content.json";
+import contentJson from "../../data/content";
 
 // Type for content.json
 interface ContentData {
