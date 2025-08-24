b26360673d133543db8a76334c92f83f3c9d8a50
EssenceArenal <essencearenalcr@gmail.com>
2025-08-24 12:05:44 -0600

    Update config.js

--- .next/config.js
+++ .next/config.js
@@ -1,17 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['firebase-admin']
+    esmExternals: 'loose'
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
+        crypto: false,
+        stream: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        os: false,
+        path: false,
       };
     }
+    
+    config.externals = config.externals || [];
+    config.externals.push({
+      'undici': 'undici',
+    });
+
     return config;
   },
 };
